27cc79ff1d1d09df5ca3a9e0338b1169cee828f3
Oumeima-IbnElfekih <oumeima.ibnelfekih@esprit.tn>
2025-02-05 20:26:41 +0100

    first commit

---
+++ src/test/java/com/jobsae/jobsae6/JobSae6ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jobsae.jobsae6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobSae6ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
