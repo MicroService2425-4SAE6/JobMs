f127eb7a24b04f79a81baf2c399aea298a76c1fb
oumeima-ibnelfekih <oumeima.ibnelfekih@esprit.tn>
2025-02-27 11:34:30 +0100

    docker config

--- src/main/java/com/jobsae/jobsae6/JobSae6Application.java
+++ src/main/java/com/jobsae/jobsae6/JobSae6Application.java
@@ -3,9 +3,11 @@ package com.jobsae.jobsae6;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class JobSae6Application {
 
 	public static void main(String[] args) {
